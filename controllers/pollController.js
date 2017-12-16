2350a9077ec3992cb8247b3576e7dfb44eb7371c
CodeDraken <codedraken@gmail.com>
2017-12-16 14:34:51 -0600

    deeper vote removal function

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -1,32 +1,24 @@
 const mongoose = require('mongoose')
 
-const {Poll} = require('../models')
+const { Poll } = require('../models')
 
 // TODO: clean up logs
 
-const updateVote = (poll, userVote, choice) => {
-  // already voted - update it
-  poll.choices[userVote.vote].votes--
-  poll.choices[choice].votes++
-
-  // update user's vote
-  poll.votes[poll.votes.indexOf(userVote)].vote = choice
-
-  return poll
-}
-
+// remove a vote and update
 const removeVote = (poll, userVote) => {
-  console.log('remove vote')
-  const voteIndex = poll.votes.indexOf(userVote)
-  const choice = poll.choices[userVote.vote]
-  poll.votes.splice(voteIndex, 1)
-  choice.votes--
+  // get all votes by the user
+  const userVotes = poll.votes.filter(vote => vote._user.equals(userVote._user))
+
+  // remove all votes by the user
+  poll.votes = poll.votes.filter(vote => !vote._user.equals(userVote._user))
+
+  // update vote numbers
+  userVotes.forEach(vote => poll.choices[vote.vote].votes--)
   poll.totalVotes--
 }
 
+// new vote
 const createVote = (poll, voter, choice) => {
-  console.log('create vote', voter, choice)
-  // new vote
   poll.choices[choice].votes++
 
   poll.votes.push({
@@ -54,7 +46,7 @@ const pollController = {
   async getPoll (req, res) {
     // get one poll by id
     try {
-      const {id} = req.params
+      const { id } = req.params
 
       const poll = await Poll.findOne({
         _id: id
@@ -69,7 +61,7 @@ const pollController = {
   async newPoll (req, res) {
     // create a new poll
     try {
-      const {title, choices} = req.body
+      const { title, choices } = req.body
       const poll = await new Poll({
         title,
         choices,
@@ -88,7 +80,7 @@ const pollController = {
   async deletePoll (req, res) {
     // delete poll and send back the deleted
     try {
-      const {id} = req.params
+      const { id } = req.params
 
       const poll = await Poll.findOneAndRemove({
         _id: id,
@@ -104,14 +96,20 @@ const pollController = {
   async vote (req, res) {
     // vote on a poll - existing option or create new one
     try {
-      const {id} = req.params
-      let {choice} = req.body
+      const { id } = req.params
+      let { choice } = req.body
       const isChoiceObj = typeof choice === 'object'
+
       const poll = await Poll.findOne({
         _id: id
       })
+
+      // if choice option exists in db grab it
       const existingChoice = isChoiceObj && poll.choices.filter(ch => ch.value === choice.value)[0]
-      let userVote = poll.votes.filter(vote => vote._user.equals(req.user._id)
+
+      // if user already voted grab their vote
+      let userVote = poll.votes.filter(vote =>
+        vote._user.equals(req.user._id)
       )[0]
 
       // remove existing vote
@@ -119,11 +117,9 @@ const pollController = {
 
       // when an object is passed for choice instead of an index
       if (isChoiceObj && existingChoice) {
-        console.log('existing choice object', existingChoice, poll.choices.indexOf(existingChoice))
         // choice exists so just use its index
         choice = poll.choices.indexOf(existingChoice)
       } else if (isChoiceObj && !existingChoice) {
-        console.log('new choice')
         // create new choice and vote
         poll.choices.push(choice)
         choice = poll.choices.length - 1
@@ -131,13 +127,6 @@ const pollController = {
 
       // add vote
       createVote(poll, req.user._id, choice)
-      // if (userVote && userVote.vote !== choice) {
-      //   console.log('update vote')
-      //   updateVote(poll, userVote, choice)
-      // } else if (!userVote) {
-      //   console.log('create vote')
-      //   createVote(poll, req.user._id, choice)
-      // }
 
       const updatedPoll = await poll.save()
 
